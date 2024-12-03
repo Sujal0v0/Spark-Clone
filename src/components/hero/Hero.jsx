3aa1aaf24f46f43f04ad5dfb6218123f68a07da8
Sujal0v0 <bajracharyasujal80@gmail.com>
2024-12-03 16:34:45 +0545

    Added image in hero section

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,17 +3,27 @@ const Hero = () => {
   return (
     <div
       id="hero"
-      className="lg:text-6xl sm:text-5xl text-5xl text-black w-[100vw] flex items-center justify-center flex-col sm:gap-10 gap-5 mt-[100px] md:py-[190px] sm:py-52 py-48 mb-5"
+      className="lg:text-6xl sm:text-5xl text-5xl text-black w-[100vw] flex items-center justify-center flex-col sm:gap-10 gap-5 mt-[100px] md:py-[190px] sm:py-52 xs:py-48 py-24 mb-5"
     >
+      <img
+        src="/left.PNG"
+        alt=""
+        className="transform lg:scale-100 md:scale-75 scale-50 absolute lg:left-0 md:-left-6 -left-12"
+      />
+      <img
+        src="/right.PNG"
+        alt=""
+        className="transform lg:scale-100 md:scale-75 scale-50 absolute lg:right-0 md:-right-6 -right-12"
+      />
       <div className="flex items-center justify-center flex-col gap-5 text-center">
-        <div className="darker px-3">Revolutionize Your Operations</div>
-        <div className="darker px-3">with our Web3 Expertise</div>
+        <div className="darker px-3 z-10">Revolutionize Your Operations</div>
+        <div className="darker px-3 z-10">with our Web3 Expertise</div>
       </div>
       <div className="flex items-center justify-center flex-col lg:text-lg text-sm text-[9px] text-medium text-center">
-        <div className="px-3">
+        <div className="px-3 z-10">
           Comprehensive solutions for blockchain integration,
         </div>
-        <div className="px-3">smart contracts, and more.</div>
+        <div className="px-3 z-10">smart contracts, and more.</div>
       </div>
       <ContactUs />
     </div>
